217d5d46e9130f543fa5248861958e9ca2a98674
Tomas Vanagas <tomasvanagas@ymail.com>
2020-04-02 00:42:41 +0300

    Main

---
+++ downloader.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+
+REMOTEUSER=""
+REMOTEHOST=""
+REMOTEPORT=""
+
+LOCALPATH=""
+REMOTEPATH=""
+
+SIZE=""
+
+while :
+do
+        FILES=$(ssh -p$REMOTEPORT -l $REMOTEUSER $REMOTEHOST "ls -lh $REMOTEPATH" | grep -v total | grep $SIZE | rev | cut -d' ' -f1 | rev | head -n 1)
+        if [ $(echo $FILES | wc -w) -gt 0 ]
+        then
+                FILES=$(echo $FILES | sed 's/ /,/g');
+                rsync -avz --remove-source-files -e "ssh -p $REMOTEPORT" $REMOTEHOST@$REMOTEHOST:$REMOTEPATH$FILES $LOCALPATH
+        fi
+        echo "[*] Sleeping..."
+        sleep 600
+done
